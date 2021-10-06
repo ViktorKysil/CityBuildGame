9864311b3f24915175d4bc6cc47aec11dce0330f
vkysil <vkysil@student.ucode.world>
2021-10-06 23:18:11 +0300

    commit 2

--- js/main.js
+++ js/main.js
@@ -17,22 +17,55 @@ function addScore() {
 //     }, 10000);
 
 function refresh() {
-    if (localStorage.getItem('userScore') === null) {
+    if (localStorage.getItem('userScore') == 0) {
         alert('Welcome to TownClicker!')
-        localStorage.setItem('userScore') = 1
+        scorePoint = 0
+        document.getElementById('score').innerHTML = scorePoint;
+        localStorage.setItem('userScore') = 0
     } else
         alert('Glad to see you again! Your previous score is ' + localStorage.getItem('userScore') + '!')
     scorePoint = document.getElementById('score').innerHTML = localStorage.getItem('userScore')
-    hired = document.getElementById('workers').innerHTML = localStorage.getItem('userWorkers')
+    hired = localStorage.getItem('userWorkers')
+    localStorage.getItem('hiredLeft')
+
+    if (localStorage.getItem('hiredLeft') > 0) {
+        count = localStorage.getItem('hiredLeft')
+        hired = 1
+        localStorage.getItem('userWorkers')
+
+        var interval = setInterval(() => {
+            count -= 1
+            document.getElementById('worker').innerHTML = 'Left: ' + count
+
+            if (count === 0) {
+                clearInterval(interval)
+                hired = 0
+                localStorage.setItem('hiredLeft', count)
+                document.getElementById('worker').innerHTML = 'No workers'
+            }
+
+            if (hired === 0) {
+                clearInterval(interval)
+                document.getElementById('worker').innerHTML = 'No workers'
+                scorePoint -= 1
+            }
+
+            scorePoint++
+            document.getElementById('score').innerHTML = scorePoint;
+            localStorage.setItem('userScore', scorePoint)
+            localStorage.setItem('hiredLeft', count)
+
+        }, 500)
+    }
 }
 
 function restart() {
     if (window.confirm('Are you sure? Game will be restarted!')) {
-      
+
         hired = 0
         localStorage.setItem('userWorkers', hired)
-        document.getElementById('worker').innerHTML = hired
-       
+        document.getElementById('worker').innerHTML = 'No workers'
+
 
         scorePoint = 0;
         document.getElementById('score').innerHTML = scorePoint;
@@ -50,33 +83,36 @@ function restart() {
 }
 
 function hire() {
-    if ((scorePoint >= workerPrice)&&(hired = 1)) {
+    if ((scorePoint >= workerPrice) && (hired === 0)) {
+        count = 20
+        localStorage.setItem('hiredLeft', count)
         hired = 1
-        document.getElementById('worker').innerHTML = hired
+
         scorePoint -= 10
         document.getElementById('score').innerHTML = scorePoint;
         localStorage.setItem('userWorkers', hired)
+
         var interval = setInterval(() => {
+            count -= 1
+            document.getElementById('worker').innerHTML = 'Left: ' + count
 
-            count += 1
-
-            if (count === 240) {
+            if (count === 0) {
                 clearInterval(interval)
                 hired = 0
-                localStorage.setItem('userWorkers', hired)
-                document.getElementById('worker').innerHTML = hired
+                localStorage.setItem('hiredLeft', count)
+                document.getElementById('worker').innerHTML = 'No workers'
             }
 
             if (hired === 0) {
                 clearInterval(interval)
+                document.getElementById('worker').innerHTML = 'No workers'
                 scorePoint -= 1
             }
 
             scorePoint++
             document.getElementById('score').innerHTML = scorePoint;
             localStorage.setItem('userScore', scorePoint)
-
-
+            localStorage.setItem('hiredLeft', count)
 
         }, 500)
     }
